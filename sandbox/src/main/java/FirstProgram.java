21d34cfae7f913ff085706a87d5a10da6558351f
Sunshine747 <denisdaniln58@gmail.com>
2016-11-03 01:50:22 +0300

    Создан класс Point, в главном классе реалзиован метод distance

--- sandbox/src/main/java/FirstProgram.java
+++ sandbox/src/main/java/FirstProgram.java
@@ -1,6 +1,17 @@
 class FirstProgram {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        Point p1 = new Point(5, 5);
+
+        Point p2 = new Point(5, -8);
+
+        System.out.println(distance(p1, p2));
+    }
+
+
+    public static double distance(Point p1, Point p2) {
+
+        return Math.sqrt(Math.pow(p1.x - p2.x, 2.0) + Math.pow(p1.y - p2.y, 2.0));
+
     }
 }
---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Администратор on 03.11.2016.
+ */
+public class Point {
+
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
