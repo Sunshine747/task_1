eda0d470f60812746c255e9f26c56d80a9967937
Sunshine747 <denisdaniln58@gmail.com>
2016-11-13 01:24:46 +0300

    Класс testContactCreation реализован как наследник от TestBase

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/TestBase.java
@@ -14,6 +14,7 @@ import java.util.concurrent.TimeUnit;
 public class TestBase {
   FirefoxDriver wd;
 
+
   public static boolean isAlertPresent(FirefoxDriver wd) {
     try {
       wd.switchTo().alert();
@@ -67,6 +68,48 @@ public class TestBase {
 
   protected void goToGroupPage() {
     wd.findElement(By.linkText("groups")).click();
+  }
+
+  protected void returnToHomePage() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+
+  protected void submitAddNewContact() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  protected void fillContactTextField(ContactData contactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
+    wd.findElement(By.name("middlename")).click();
+    wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddleName());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
+    wd.findElement(By.name("nickname")).click();
+    wd.findElement(By.name("nickname")).clear();
+    wd.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
+    wd.findElement(By.name("title")).click();
+    wd.findElement(By.name("title")).clear();
+    wd.findElement(By.name("title")).sendKeys(contactData.getTitle());
+    wd.findElement(By.name("company")).click();
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(contactData.getHomePhoneNumber());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(contactData.getMobilePhoneNumber());
+  }
+
+  protected void goToAddNewContact() {
+    wd.findElement(By.linkText("add new")).click();
   }
 
   @AfterMethod
